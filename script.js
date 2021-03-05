bff83ab8153b1eabb21f058b2059e4f03a7a4de9
SerhiiTekhnev <seolteh@gmail.com>
2021-03-05 14:42:58 +0200

    By string

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let button = document.querySelector('button[type=submit]')
 button.addEventListener('click', function() {
     let InputWord = document.querySelector('form input[name=word]').value
-    let a = InputWord.split("")
-    if(a.length <= 10) {
+    if(InputWord.length <= 10) {
         alert(InputWord)
     } else {
-        alert(a[0] + (a.length-2) + a[a.length-1])
+        alert(InputWord[0] + (InputWord.length-2) + InputWord[InputWord.length-1])
     }
 })
